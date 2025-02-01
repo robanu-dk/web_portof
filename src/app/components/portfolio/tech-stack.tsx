0c8c609bbb75ec8293f11d2ee19841ff5ef30b4f
robanu <rd.scout06@gmail.com>
2025-02-01 21:40:02 +0700

    (adjust) display tech stacl

--- src/app/components/portfolio/tech-stack.tsx
+++ src/app/components/portfolio/tech-stack.tsx
@@ -1,25 +1,25 @@
 import Image from 'next/image';
 
 interface PropsTools {
-    tools: {
+    tech_stacks: {
         id: number,
         title: string,
         icon_source: string,
     }[],
 }
 
-export default function Tools({ tools }: PropsTools) {
+export default function TechStack({ tech_stacks }: PropsTools) {
     return (
-        <section className="section" id="tools">
+        <section className="section" id="tech-stacks">
             <div className="container text-center">
                 <p className="section-subtitle">What I Use ?</p>
-                <h6 className="section-title mb-6">Tools</h6>
+                <h6 className="section-title mb-6">Tech Stacks</h6>
                 {/* <!-- row --> */}
                 <div className="row">
                     {
-                        tools.map((item) => {
+                        tech_stacks.map((item) => {
                             return (
-                                <div key={item.id} className="col-md-6 col-lg-2 mt-4">
+                                <div key={item.id} className="col-sm-4 col-md-3 col-lg-2 mt-4">
                                     <div className="d-flex justify-content-center">
                                         <div className="body">
                                             <Image src={item.icon_source} alt={item.title} key={item.id} width={100} height={100} className="icon" />
