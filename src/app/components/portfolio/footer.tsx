4f2f0a7980474a39ace675c3ffd999a207a36f4e
robanu <rd.scout06@gmail.com>
2025-02-02 10:50:49 +0700

    (feat) footer

--- src/app/components/portfolio/footer.tsx
+++ src/app/components/portfolio/footer.tsx
@@ -1,15 +1,14 @@
+import { FaLinkedin, FaInstagram, FaGithub } from 'react-icons/fa'; 
+
 export default function Footer() {
     return (
         <div className="container">
             <footer className="footer">
-                <p className="mb-0">Copyright yeear &copy; <a href="#">DevCRUD</a></p>
-                <div className="social-links text-right m-auto ml-sm-auto">
-                    <a href="#" className="link"><i className="ti-facebook"></i></a>
-                    <a href="#" className="link"><i className="ti-twitter-alt"></i></a>
-                    <a href="#" className="link"><i className="ti-google"></i></a>
-                    <a href="#" className="link"><i className="ti-pinterest-alt"></i></a>
-                    <a href="#" className="link"><i className="ti-instagram"></i></a>
-                    <a href="#" className="link"><i className="ti-rss"></i></a>
+                <p className="mb-0">Copyright Robanu Dakhayin &copy; <a href="#">{(new Date()).getFullYear()}</a></p>
+                <div className="row justify-content-end gap-default social-links text-right m-auto ml-sm-auto">
+                    <a href="https://github.com/robanu-dk" target='_blank' className="link d-flex justify-content-center align-items-center"><FaGithub className='w-2/3 h-2/3' /></a>
+                    <a href="https://www.linkedin.com/in/robanu-dakhayin/" target='_blank' className="link d-flex justify-content-center align-items-center"><FaLinkedin className='w-2/3 h-2/3' /></a>
+                    <a href="https://www.instagram.com/robanu_dakhayin/" target='_blank' className="link d-flex justify-content-center align-items-center"><FaInstagram className='w-2/3 h-2/3' /></a>
                 </div>
             </footer>
         </div>
