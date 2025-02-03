af879a42e87b32c90651b5045b5c390c16459752
robanu <rd.scout06@gmail.com>
2025-02-03 22:13:42 +0700

    (feat) contact me via whatsapp

--- src/app/components/portfolio/contact.tsx
+++ src/app/components/portfolio/contact.tsx
@@ -1,23 +1,57 @@
+'use client';
+
+import { useEffect } from 'react';
+import { FaWhatsapp } from 'react-icons/fa';
+
+const WHATSAPP_URL = 'https://wa.me/+6288228400856';
+
+function SubmitForm() {
+    useEffect(() => {
+        const form = document.getElementById('contact-me-form');
+
+        form?.addEventListener('submit', function (e: { preventDefault: () => void; }) {
+            e.preventDefault();
+
+            const data = new FormData(this as HTMLFormElement);
+
+            const dataObj: { [key: string]: string } = {};
+
+            data.forEach((value, key) => {
+                dataObj[key as string] = value as string;
+            })
+
+            const message = `*Nama:* ${dataObj.name}%0A*Pesan:*%0A${dataObj.message}`;
+
+            window.open(`${WHATSAPP_URL}?text=${message}`);
+        });
+    });
+}
+
 export default function ContactMe() {
+
+    // Function untuk handling submit form
+    SubmitForm();
+
     return (
         <section className="section" id="contact">
             <div className="container text-center">
                 <p className="section-subtitle">How can you communicate?</p>
                 <h6 className="section-title mb-5">Contact Me</h6>
                 {/* <!-- contact form --> */}
-                <form action="" className="contact-form col-md-10 col-lg-8 m-auto">
+                <form id='contact-me-form' className="contact-form col-md-10 col-lg-8 m-auto">
                     <div className="form-row">
-                        <div className="form-group col-sm-6">
-                            <input type="text" size={50} className="form-control" placeholder="Your Name" required />
-                        </div>
-                        <div className="form-group col-sm-6">
-                            <input type="email" className="form-control" placeholder="Enter Email" required />
+                        <div className="form-group col-sm-12">
+                            <input name='name' type="text" className="form-control" placeholder="Your Name" required />
                         </div>
                         <div className="form-group col-sm-12">
-                            <textarea name="comment" id="comment" rows={6} className="form-control" placeholder="Write Something"></textarea>
+                            <textarea name="message" id="message" rows={6} className="form-control" placeholder="Write Something"></textarea>
                         </div>
                         <div className="form-group col-sm-12 mt-3">
-                            <input type="submit" value="Send Message" className="btn btn-outline-primary rounded" />
+                            <button type="submit" className="btn btn-outline-primary rounded">
+                                <div className='flex gap-default align-items-center'>
+                                    <FaWhatsapp />Send Message
+                                </div>
+                            </button>
                         </div>
                     </div>
                 </form> {/** <!-- end of contact form --> */}
