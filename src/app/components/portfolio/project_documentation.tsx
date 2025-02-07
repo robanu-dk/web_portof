5b9cc8ce2a7646e88057992db70e7480c1a9cc34
robanu <rd.scout06@gmail.com>
2025-02-07 21:38:28 +0700

    (fix) split carousel into a component

---
+++ src/app/components/portfolio/project_documentation.tsx
@@ -0,0 +1,101 @@
+'use client'
+
+import Image from 'next/image';
+import { MouseEvent, useState } from 'react';
+
+interface ProjectDocumentationProps {
+    project_documentations: {
+        id: number,
+        description: string,
+        image_source: string,
+    }[],
+    project_id: number,
+}
+
+export default function ProjectDocumentationCarousel({ project_documentations, project_id }: ProjectDocumentationProps) {
+    const [show, setShow] = useState<number>(0);
+    const [count_project_document_old, setCountProjectDocOld] = useState<number>(0);
+
+    // set the first value of the old project document count when first rendered
+    if (count_project_document_old === 0) {
+        setCountProjectDocOld(project_documentations.length);
+    }
+
+    // detect if there are changes to project documentation
+    if (count_project_document_old != project_documentations.length) {
+        setCountProjectDocOld(project_documentations.length);
+        setShow(0);
+    }
+    
+    const scrollTo = (id_target: string) => {
+        const target = document.getElementById(id_target);
+        window.scrollTo({
+            top: target ? target.offsetTop * 1.1 : 0,
+            behavior: 'smooth',
+        });
+    }
+
+    const nextSlide = (e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>, id_target: string) => {
+        e.preventDefault();
+        setShow(show == (project_documentations.length - 1) ? 0 : show + 1);
+
+        scrollTo(id_target.replaceAll('#', ''));
+    }
+
+    const previousSlide = (e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>, id_target: string) => {
+        e.preventDefault();
+        setShow(show == 0 ? (project_documentations.length - 1) : show - 1);
+
+        scrollTo(id_target.replaceAll('#', ''));
+    }
+
+    const handleIndicatorClick = (e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>, index: number, id_target: string) => {
+        e.preventDefault();
+        setShow(index);
+
+        scrollTo(id_target.replaceAll('#', ''));
+    }
+
+    return (
+        <div id="default-carousel" className="relative w-full">
+            {/* <!-- Carousel wrapper --> */}
+            <div className="relative h-full overflow-hidden rounded-lg">
+                {
+                    project_documentations.map((item, index) =>
+                        <div key={index} className={`${show == index ? 'transition-opacity duration-500 ease-in-out opacity-100' : 'opacity-0'}`}>
+                            <p className={`${show != index ? 'hidden' : ''} text-start font-normal text-sm xl:text-base 2xl:text-lg`}>{item.description}</p>
+                            <Image className={`${show != index ? 'hidden' : ''} relative block w-full rounded-sm`} src={item.image_source} height={4000} width={4000} alt={item.image_source.split('/').reverse()[0]} priority={false} />
+                        </div>
+                    )
+                }
+                {/* <!-- Slider controls --> */}
+                {
+                    project_documentations.length > 1 ? <>
+                        <a href={`#carousel-section-${project_id}`} type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={(e) => { previousSlide(e, `carousel-section-${project_id}`) }}>
+                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
+                                </svg>
+                                <span className="sr-only">Previous</span>
+                            </span>
+                        </a>
+                        <a href={`#carousel-section-${project_id}`} type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={(e) => { nextSlide(e, `carousel-section-${project_id}`) }}>
+                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+                                </svg>
+                                <span className="sr-only">Next</span>
+                            </span>
+                        </a>
+                    </> : null
+                }
+                {/* <!-- Slider indicators --> */}
+                <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                    {
+                        project_documentations.length > 1 ? project_documentations.map((item, index) => <a key={index} type="button" className={`w-3 h-3 rounded-full ${index == show ? 'bg-white' : 'bg-gray-400'}`} aria-current="true" aria-label={`Slide ${index + 1}`} onClick={(e) => handleIndicatorClick(e, index, `#carousel-section-${project_id}`)}></a>) : null
+                    }
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/components/portfolio/projects.tsx
+++ src/app/components/portfolio/projects.tsx
@@ -1,7 +1,4 @@
-'use client';
-
-import Image from 'next/image';
-import { useState } from 'react';
+import ProjectDocumentationCarousel from "./project_documentation"
 
 interface ProjectProps {
     id: number,
@@ -22,81 +19,6 @@ interface ProjectsProps {
     projects: ProjectProps[],
 }
 
-function ProjectDocumentationCarousel(project_documentations: { id: number, description: string, image_source: string }[], key: number) {
-    const [show, setShow] = useState(0);
-
-    const scrollTo = (id_target: string) => {
-        const target = document.getElementById(id_target);
-        window.scrollTo({
-            top: target ? target.offsetTop * 1.1 : 0,
-            behavior: 'smooth',
-        });
-    }
-
-    const nextSlide = (e: { preventDefault: () => void; }, id_target: string) => {
-        e.preventDefault();
-        setShow(show == (project_documentations.length - 1) ? 0 : show + 1);
-
-        scrollTo(id_target.replaceAll('#', ''));
-    }
-
-    const previousSlide = (e: { preventDefault: () => void; }, id_target: string) => {
-        e.preventDefault();
-        setShow(show == 0 ? (project_documentations.length - 1) : show - 1);
-
-        scrollTo(id_target.replaceAll('#', ''));
-    }
-
-    const handleIndicatorClick = (index: number, id_target: string) => {
-        setShow(index);
-
-        scrollTo(id_target);
-    }
-
-    return (
-        <div id="default-carousel" className="relative w-full">
-            {/* <!-- Carousel wrapper --> */}
-            <div className="relative h-full overflow-hidden rounded-lg">
-                {
-                    project_documentations.map((item, index) =>
-                        <div key={index} className={`${show == index ? 'transition-opacity duration-500 ease-in-out opacity-100' : 'opacity-0'}`}>
-                            <p className={`${show != index ? 'hidden' : ''} text-start font-normal text-sm xl:text-base 2xl:text-lg`}>{item.description}</p>
-                            <Image className={`${show != index ? 'hidden' : ''} relative block w-full rounded-sm`} src={item.image_source} height={4000} width={4000} alt={item.image_source.split('/').reverse()[0]} priority={false} />
-                        </div>
-                    )
-                }
-                {/* <!-- Slider controls --> */}
-                {
-                    project_documentations.length > 1 ? <>
-                        <a href={`#carousel-section-${key}`} type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={(e) => { previousSlide(e, `carousel-section-${key}`) }}>
-                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
-                                </svg>
-                                <span className="sr-only">Previous</span>
-                            </span>
-                        </a>
-                        <a href={`#carousel-section-${key}`} type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={(e) => { nextSlide(e, `carousel-section-${key}`) }}>
-                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-                                </svg>
-                                <span className="sr-only">Next</span>
-                            </span>
-                        </a>
-                    </> : null
-                }
-                {/* <!-- Slider indicators --> */}
-                <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-                    {
-                        project_documentations.length > 1 ? project_documentations.map((item, index) => <a key={index} type="button" className={`w-3 h-3 rounded-full ${index == show ? 'bg-white' : 'bg-gray-400'}`} aria-current="true" aria-label={`Slide ${index + 1}`} onClick={() => handleIndicatorClick(index, `#carousel-section-${key}`)}></a>) : null
-                    }
-                </div>
-            </div>
-        </div>
-    );
-}
-
 export default function Projects({ projects }: ProjectsProps) {
     return (
         <section className="section" id="projects">
@@ -106,7 +28,7 @@ export default function Projects({ projects }: ProjectsProps) {
                 {/* <!-- row --> */}
                 {
                     projects.map((project, key) =>
-                        <div key={`project-${key}`} id={`carousel-section-${key}`} className='mb-3'>
+                        <div key={`project-${key}`} id={`carousel-section-${project.id}`} className='mb-3'>
                             <div className="card card-projects rounded-sm hover:-translate-y-2 hover:shadow-lg">
                                 <div className='p-2'>
                                     <h4 className='text-start mb-3 mt-2'>{project.title} ({project.project_category})</h4>
@@ -132,7 +54,7 @@ export default function Projects({ projects }: ProjectsProps) {
                                     }
                                     <p className='text-start font-normal text-sm xl:text-base 2xl:text-lg'><b>Documentation:</b></p>
                                     {
-                                        ProjectDocumentationCarousel(project.documentation, key)
+                                        <ProjectDocumentationCarousel project_documentations={project.documentation} project_id={project.id} />
                                     }
                                 </div>
                             </div>
